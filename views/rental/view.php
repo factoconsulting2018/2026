1ff9496c8e89eb363ad18d1843d3e1b632afe609
Facto Consulting <factoconsulting2018@gmail.com>
2025-11-03 13:27:39 -0600

    Cambiar acciÃ³n de descarga de PDF para generar orden en vivo

--- views/rental/view.php
+++ views/rental/view.php
@@ -148,7 +148,7 @@ $this->params['breadcrumbs'][] = $this->title;
                             'style' => 'background: linear-gradient(135deg, #22487a 0%, #0d001e 100%); border: none;'
                         ]) ?>
 
-                        <?= Html::a('<span class="material-symbols-outlined" style="font-size: 18px; vertical-align: middle; margin-right: 4px;">picture_as_pdf</span>Orden PDF', ['/pdf/download-rental', 'id' => $model->id], [
+                        <?= Html::a('<span class="material-symbols-outlined" style="font-size: 18px; vertical-align: middle; margin-right: 4px;">picture_as_pdf</span>Orden PDF', ['/pdf/rental-order', 'id' => $model->id], [
                             'class' => 'btn btn-info btn-lg',
                             'target' => '_blank'
                         ]) ?>
